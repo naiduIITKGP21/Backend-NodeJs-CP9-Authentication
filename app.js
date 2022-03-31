a7e0e1b2744d87a54881fa8f503b0cfc93f5c697
ammkutti2310 <naiduiitkgp21@gmail.com>
2022-03-31 01:49:22 +0000

    Database initialization

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const { open } = require("sqlite");
+const path = require("path");
+const sqlite3 = require("sqlite3");
+
+const bcrypt = require("bcrypt");
+
+let db = null;
+const dbPath = path.join(__dirname, "userData.db");
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () =>
+      console.log("server running at http://localhost:3000/")
+    );
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
