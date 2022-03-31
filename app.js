62478996b47426d1acdd882b64b36b06dcb6edd6
ammkutti2310 <naiduiitkgp21@gmail.com>
2022-03-31 02:43:41 +0000

    API 1:user RegistrationAPI

--- app.js
+++ app.js
@@ -26,3 +26,29 @@ const initializeDBAndServer = async () => {
   }
 };
 initializeDBAndServer();
+
+//API 1: user registration
+app.post("/register", async (request, response) => {
+  const { username, name, password, gender, location } = request.body;
+  const userCheckingQuery = `SELECT * FROM user WHERE username = '${username}';`;
+  const existingUser = await db.get(userCheckingQuery);
+  if (existingUser === undefined) {
+    if (password.length >= 5) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const createUserQuery = `
+    INSERT INTO user(username, name, password, gender, location)
+    VALUES
+    ('${username}', '${name}', '${hashedPassword}','${gender}', '${location}');`;
+      await db.run(createUserQuery);
+      response.send(" Successful registration of the registrant");
+      console.log(`${username} user registered successfully`);
+    } else {
+      response.status(400);
+      response.send("Password is too short");
+    }
+  } else {
+    response.status(400);
+    response.send("User already exists");
+    console.log(`${existingUser.username} user already exits`);
+  }
+});
